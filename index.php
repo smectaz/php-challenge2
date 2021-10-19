c0881c4929180c96d8d3771268905ef0b9913098
smectaz <90684643+smectaz@users.noreply.github.com>
2021-10-19 19:01:04 +0200

    second write of index

--- index.php
+++ index.php
@@ -10,3 +10,24 @@ echo writeSecretSentence('L\'enclume', 'au forgeron');
 echo writeSecretSentence('Le programme', 'au développeur');
 echo writeSecretSentence('La serrure', 'au serrurier');
 echo writeSecretSentence('Le coffre fort', 'au voleur');
+
+//deuxième façon
+
+function writeSecretSentence2(string $objet, string $metier)
+{
+    return "$objet s’incline face  $metier";
+}
+
+echo writeSecretSentence2('L\'enclume', 'au forgeron');
+
+echo '<br>';
+
+echo writeSecretSentence2('Le programme', 'au développeur');
+
+echo '<br>';
+
+echo writeSecretSentence2('La serrure', 'au serrurier');
+
+echo '<br>';
+
+echo writeSecretSentence2('Le coffre fort', 'au voleur');
