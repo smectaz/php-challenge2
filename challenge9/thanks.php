146e5d3277d75b1b70d0746383b8a09a0da2599f
smectaz <90684643+smectaz@users.noreply.github.com>
2021-11-03 18:18:51 +0100

    challenge9

---
+++ challenge9/thanks.php
@@ -0,0 +1,65 @@
+<?php
+
+function Test($result)
+{
+    $result = stripslashes($result);
+    $result = trim($result);
+    $result = htmlspecialchars($result);
+    return $result;
+}
+
+if ($_POST) {
+
+/*echo "
+<pre>";
+print_r($_POST);
+echo "</pre>
+";}
+var_dump($_POST);*/
+    if (empty($_POST['nom'])) {
+        echo ("Vous devez mettre un nom<br>");
+    } else {
+        $nom = Test($_POST['nom']);
+
+    }
+
+    if (empty($_POST['prenom'])) {
+        echo ("Vous devez mettre un prenom<br>");
+    } else {
+        $prenom = Test($_POST['prenom']);
+
+    }
+
+    if (empty($_POST['mail'])) {
+        echo ("Vous devez mettre un email<br>");
+    } else {
+
+        if (!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) {
+            echo "le format de l'email est incorrecte<br>";
+        } else {
+            $mail = Test($_POST['mail']);
+        }
+    }
+
+    if (empty($_POST['tel'])) {
+        echo ("Vous devez mettre un numéro de téléphone pour étre recontacter<br>");
+    } else {
+        $tel = Test($_POST['tel']);
+    }
+
+    if (empty($_POST['sujet'])) {
+        echo ("Vous devez mettre le service concerné par votre message<br>");
+    } else {
+        $sujet = Test($_POST['sujet']);
+
+    }
+
+    if (empty($_POST['message'])) {
+        echo ("Vous devez mettre un message expliquant votre demande<br>");
+    } else {
+        $message = Test($_POST['message']);
+
+    }
+    if (!empty($nom) && !empty($prenom) && !empty($tel) && !empty($mail) && !empty($sujet) && !empty($message)) {
+        echo ("Merci " . $prenom . " " . $nom . " de nous avoir contacté à propos d'un problème " . $sujet . ". <br> Un de nos conseiller vous recontactera soit à l'adresse mail " . $mail . " ou par téléphone au " . $tel . " dans les plus bref délais pour traiter votre demande:<br>" . $message . ".");}
+}
