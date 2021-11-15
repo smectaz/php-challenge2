98dc5235b6372a229b07ed591f085847b1de248b
smectaz <90684643+smectaz@users.noreply.github.com>
2021-11-15 17:35:09 +0100

    update challenge-12

--- challenge-12/index.php
+++ challenge-12/index.php
@@ -1,4 +1,4 @@
-<?php include('inc/head.php'); ?>
+<?php include 'inc/head.php';?>
 
 <!--    C'est ici que tu vas devoir afficher le contenu de tes repertoires et fichiers. -->
 
@@ -15,7 +15,8 @@ if (isset($_POST["content"])) {
 }
 
 // récupérer tous les fichiers d'un dossier de manière récursive
-function getDirContents($dir, &$results = array()) {
+function getDirContents($dir, &$results = array())
+{
     $files = scandir($dir);
 
     foreach ($files as $key => $value) {
@@ -36,70 +37,70 @@ function getDirContents($dir, &$results = array()) {
 $files = getDirContents("files");
 
 foreach ($files as $file) {
-    echo '<a href=?f='.$file.'>';
-    echo basename($file)."<br>";
+    echo '<a href=?f=' . $file . '>';
+    echo basename($file) . "<br>";
     echo '</a>';
 }
 
 if (isset($_GET["f"]) && str_contains($_GET["f"], ".jpg")) {
     ?>
-    <form action="" method="get">
-        <div>
-            <?php echo "<img src=" . str_replace("/Users/moi/OneDrive/DonkeySchool/php/php_files_handling_ressources", "", $_GET["f"]) . " style='width:100%' alt=" . basename($_GET["f"]) . " >"; ?>
-            <label for="">Delete image:</label>
-            <?php
-            echo '<a href=?d='.$_GET["f"].'>';
-            echo basename($_GET["f"])."<br>";
-            echo '</a>';
-            ?>
-        </div>
-    </form>
-    <?php
+<form action="" method="get">
+    <div>
+        <?php echo "<img src=" . str_replace("/xampp/htdocs/mon-premier-script-php/php-challenge2/challenge-12", "", $_GET["f"]) . " style='width:100%' alt=" . basename($_GET["f"]) . " >"; ?>
+        <label for="">Delete image:</label>
+        <?php
+echo '<a href=?d=' . $_GET["f"] . '>';
+    echo basename($_GET["f"]) . "<br>";
+    echo '</a>';
+    ?>
+    </div>
+</form>
+<?php
 } elseif (isset($_GET["f"]) && !is_dir($_GET["f"])) {
     $content = file_get_contents($_GET["f"]);
     ?>
-    <form action="" method="post">
-        <div>
-            <label for="">Edit file:</label>
-            <textarea name="content" style="width:100%;height: 400px;">
+<form action="" method="post">
+    <div>
+        <label for="">Edit file:</label>
+        <textarea name="content" style="width:100%;height: 400px;">
                 <?php echo $content; ?>
             </textarea>
-            <input type="submit" value="Submit changes"/>
-            <label for="">Delete file:</label>
-            <?php
-            echo '<a href=?d='.$_GET["f"].'>';
-            echo basename($_GET["f"])."<br>";
-            echo '</a>';
-            ?>
-        </div>
-    </form>
-    <?php
+        <input type="submit" value="Submit changes" />
+        <label for="">Delete file:</label>
+        <?php
+echo '<a href=?d=' . $_GET["f"] . '>';
+    echo basename($_GET["f"]) . "<br>";
+    echo '</a>';
+    ?>
+    </div>
+</form>
+<?php
 } elseif (isset($_GET["f"])) {
     ?>
-    <form action="" method="post">
-        <div>
-            <label for="">Delete directory:</label>
-            <?php
-            echo '<a href=?d='.$_GET["f"].'>';
-            echo basename($_GET["f"])."<br>";
-            echo '</a>';
-            ?>
-        </div>
-    </form>
-    <?php
+<form action="" method="post">
+    <div>
+        <label for="">Delete directory:</label>
+        <?php
+echo '<a href=?d=' . $_GET["f"] . '>';
+    echo basename($_GET["f"]) . "<br>";
+    echo '</a>';
+    ?>
+    </div>
+</form>
+<?php
 }
 
 if (isset($_GET["d"])) {
     ?>
-    <form action="delete.php" method="get">
-        <div>
-            <label for="">Do you really want to delete <?php echo basename($_GET["d"])?>?</label>
-            <br>
-            <input name="delete" type="hidden" value="<?php echo $_GET["d"]?>"/>
-            <input type="submit" value="Yes, please delete <?php echo basename($_GET["d"]) ?>"/>
-        </div>
-    </form>
+<form action="delete.php" method="get">
+    <div>
+        <label for="">Do you really want to delete <?php echo basename($_GET["d"]) ?>?</label>
+        <br>
+        <input name="delete" type="hidden" value="<?php echo $_GET["d"] ?>" />
+        <input type="submit" value="Yes, please delete <?php echo basename($_GET["d"]) ?>" />
+    </div>
+</form>
 <?php
 }
 
-include('inc/foot.php');
+include 'inc/foot.php';
